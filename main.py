de616d46f48b436cfaa05ba9c3a01b29e50f5ef8
Jonah Mackey <jonahmackey699@gmail.com>
2023-04-02 00:34:23 -0400

    IRT code

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from part_a import knn, matrix_factorization
+from part_a import knn, matrix_factorization, item_response
 
 if __name__ == "__main__":
     # Part A: Question 1.
-    knn.main()
+    # knn.main()
     
     # Question 2.
+    item_response.main()
     
     # Question 3. (i)
-    matrix_factorization.main()
+    # matrix_factorization.main()
     
     # Question 4.
--- part_a/item_response.py
+++ part_a/item_response.py
@@ -11,7 +11,7 @@ def sigmoid(x):
     return np.exp(x) / (1 + np.exp(x))
 
 
-def neg_log_likelihood(matrix, theta, beta):
+def neg_log_likelihood(data, theta, beta):
     """ Compute the negative log-likelihood.
 
     You may optionally replace the function arguments to receive a matrix.
@@ -27,19 +27,16 @@ def neg_log_likelihood(matrix, theta, beta):
     # Implement the function as described in the docstring.             #
     #####################################################################
     log_lklihood = 0.
-    for i in range(matrix.shape[0]):
-        for j in range(matrix.shape[1]):
-            c_ij = matrix[i, j]
-            first_term = -c_ij * np.log(np.exp(beta[j] - theta[i]) + 1)
-            second_term = -(1 - c_ij) * np.log(1 + np.exp(theta[i] - beta[j]))
-            log_lklihood += first_term + second_term
+    for user_idx, question_idx, is_correct in zip(data["user_id"], data["question_id"], data["is_correct"]):
+        x = beta[question_idx] - theta[user_idx]
+        log_lklihood += -is_correct * np.logaddexp(0, x) - (1 - is_correct) * np.logaddexp(0, -x)
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
     return -log_lklihood
 
 
-def update_theta_beta(matrix, lr, theta, beta):
+def update_theta_beta(data, lr, theta, beta):
     """ Update theta and beta using gradient descent.
 
     You are using alternating gradient descent. Your update should look:
@@ -60,32 +57,23 @@ def update_theta_beta(matrix, lr, theta, beta):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    # TODO: Correct? (Check in OH)
-
-    for idx in range(len(theta)):
-        theta_derivative = 0.
-        for i in range(matrix.shape[0]):
-            for j in range(matrix.shape[1]):
-                c_ij = matrix[i, j]
-                if c_ij != np.nan:
-                    theta_derivative += ...  # TODO
-        theta[idx] = theta[idx] - lr * theta_derivative
-
-    for idx in range(len(beta)):
-        beta_derivative = 0.
-        for i in range(matrix.shape[0]):
-            for j in range(matrix.shape[1]):
-                c_ij = matrix[i, j]
-                if c_ij != np.nan:
-                    beta_derivative += ...  # TODO
-        beta[idx] = beta[idx] - lr * beta_derivative
+    # TODO: what about nan entries in sparse matrix?
+    # Gradient step for theta
+    for user_idx, question_idx, is_correct in zip(data["user_id"], data["question_id"], data["is_correct"]):
+        x = theta[user_idx] - beta[question_idx] 
+        theta[user_idx] += lr * (is_correct - sigmoid(x))
+        
+    # Gradient step for beta
+    for user_idx, question_idx, is_correct in zip(data["user_id"], data["question_id"], data["is_correct"]):
+        x = theta[user_idx] - beta[question_idx] 
+        beta[question_idx] -= lr * (is_correct - sigmoid(x))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
     return theta, beta
 
 
-def irt(matrix, val_data, lr, iterations):
+def irt(train_data, val_data, lr, iterations):
     """ Train IRT model.
 
     You may optionally replace the function arguments to receive a matrix.
@@ -98,36 +86,49 @@ def irt(matrix, val_data, lr, iterations):
     :param iterations: int
     :return: (theta, beta, val_acc_lst)
     """
-    # TODO: Play with initialization (ASK TA?)
-    theta = np.zeros(matrix.shape[0])
-    beta = np.zeros(matrix.shape[1])
+    # Initialize theta and beta
+    theta = np.random.randn(542) 
+    beta = np.random.randn(1774)
 
     val_acc_lst = []
     trn_acc_lst = []
-    neg_llds = []
+    trn_neg_llds = []
+    val_neg_llds = []
 
     for i in range(iterations):
-        neg_lld = neg_log_likelihood(matrix, theta=theta, beta=beta)
-        neg_llds.append(neg_lld)
+        # print(f'ITERATION {i}:')
+        
+        # update theta and beta
+        theta, beta = update_theta_beta(train_data, lr, theta, beta)
+        
+        # negative likelihood on training set
+        trn_neg_lld = neg_log_likelihood(data=train_data, theta=theta, beta=beta)
+        trn_neg_llds.append(trn_neg_lld)
 
-        # TODO: Does it make sense to evaluate on train set here?
-        trn_score = evaluate(data=matrix, theta=theta, beta=beta)
+        # accuracy on training set
+        trn_score = evaluate(data=train_data, theta=theta, beta=beta)
         trn_acc_lst.append(trn_score)
+        
+        # print("TRN NLLK: {} \t TRN Score: {}".format(trn_neg_lld, trn_score))
 
+        # negative likelihood on validation set
+        val_neg_lld = neg_log_likelihood(data=val_data, theta=theta, beta=beta)
+        val_neg_llds.append(val_neg_lld)
+
+        # accuracy on validation set
         val_score = evaluate(data=val_data, theta=theta, beta=beta)
         val_acc_lst.append(val_score)
-        print("NLLK: {} \t Score: {}".format(neg_lld, val_score))
-        theta, beta = update_theta_beta(matrix, lr, theta, beta)
 
-    # TODO: You may change the return values to achieve what you want.
-    return theta, beta, val_acc_lst, trn_acc_lst, neg_llds
+        # print("VAL NLLK: {} \t VAL Score: {}".format(val_neg_lld, val_score), '\n')
+        
+    return theta, beta, val_acc_lst, trn_acc_lst, trn_neg_llds, val_neg_llds
 
 
 def evaluate(data, theta, beta):
     """ Evaluate the model given data and return the accuracy.
+    
     :param data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
-
     :param theta: Vector
     :param beta: Vector
     :return: float
@@ -143,11 +144,11 @@ def evaluate(data, theta, beta):
 
 
 def main():
-    train_data = load_train_csv("../data")
+    train_data = load_train_csv("./data")
     # You may optionally use the sparse matrix.
-    sparse_matrix = load_train_sparse("../data")
-    val_data = load_valid_csv("../data")
-    test_data = load_public_test_csv("../data")
+    sparse_matrix = load_train_sparse("./data")
+    val_data = load_valid_csv("./data")
+    test_data = load_public_test_csv("./data")
 
     #####################################################################
     # TODO:                                                             #
@@ -155,23 +156,23 @@ def main():
     # code, report the validation and test accuracy.                    #
     #####################################################################
     # Set the hyperparameters.
-    lr = ...  # TODO
-    iterations = ...  # TODO
+    lr = 0.01
+    iterations = 100
 
     # Train the model.
-    theta, beta, val_acc_lst, trn_acc_lst, neg_llds = irt(sparse_matrix, val_data, lr, iterations)
+    theta, beta, val_acc_lst, trn_acc_lst, trn_neg_llds, val_neg_llds = irt(train_data, val_data, lr, iterations)
 
     # Evaluate on the validation and test set.
     final_val_acc = evaluate(data=val_data, theta=theta, beta=beta)
     final_test_acc = evaluate(data=test_data, theta=theta, beta=beta)
+    
     if final_val_acc != val_acc_lst[-1]:
         print('\033[93m' + f'Final validation accuracy: {final_val_acc} is not the same as '
                            f'the last element in the list: {val_acc_lst[-1]}' + '\033[0m')
     print('-' * 30)
     print(f'Hyperparameters: lr={lr}, iterations={iterations}')
     print(f'Final validation accuracy: {final_val_acc}')
-    # TODO: Print test accuracy at the end.
-    # print(f'Final test accuracy: {final_test_acc}')
+    print(f'Final test accuracy: {final_test_acc}')
     print('-' * 30)
 
     def create_and_save_plot(x, y, x_label, y_label, title, file_name):
@@ -183,16 +184,18 @@ def main():
         plt.clf()
 
     # Plot all lists.
-    create_and_save_plot(range(iterations), val_acc_lst,
-                         'Iterations', 'Validation Accuracy',
-                         'Validation Accuracy vs. Iterations', 'val_acc.png')
     create_and_save_plot(range(iterations), trn_acc_lst,
                          'Iterations', 'Training Accuracy',
-                         'Training Accuracy vs. Iterations', 'trn_acc.png')
-    create_and_save_plot(range(iterations), neg_llds,
+                         'Training Accuracy vs. Iterations', './part_a/images/irt_trn_acc.png')
+    create_and_save_plot(range(iterations), val_acc_lst,
+                         'Iterations', 'Validation Accuracy',
+                         'Validation Accuracy vs. Iterations', './part_a/images/irt_val_acc.png')
+    create_and_save_plot(range(iterations), trn_neg_llds,
                          'Iterations', 'Negative Log Likelihood',
-                         'Negative Log Likelihood vs. Iterations', 'nllk.png')
-
+                         'Training Negative Log Likelihood vs. Iterations', './part_a/images/irt_trn_nllk.png')
+    create_and_save_plot(range(iterations), val_neg_llds,
+                         'Iterations', 'Negative Log Likelihood',
+                         'Validation Negative Log Likelihood vs. Iterations', './part_a/images/irt_val_nllk.png')
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -202,7 +205,7 @@ def main():
     # Implement part (d)                                                #
     #####################################################################
     # TODO: Switch to deterministic mode in future.
-    js = np.random.choice(sparse_matrix.shape[1], 3, replace=False)
+    # js = np.random.choice(sparse_matrix.shape[1], 3, replace=False)
     # TODO: Question for TA: What do you mean vary theta for a question?
     #  Theta is student specific so I'm not sure what you mean by varying theta for a question.
     #  Do we report all students somehow? Like how vary a whole vector in one axis?
